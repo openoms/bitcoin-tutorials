3e499333af0ae97b095a0aa737111ed200fddf98
bluecell296 <43343391+openoms@users.noreply.github.com>
2019-07-11 16:01:46 +0100

    remove electrs. service installation as it is a duplicate

--- electrs/3_Nginx_and_Certbot_for_SSL.sh
+++ electrs/3_Nginx_and_Certbot_for_SSL.sh
@@ -85,36 +85,6 @@ sudo systemctl enable certbot.timer
 
 echo ""
 echo "***"
-echo "Type the PASSWORD B of your RaspiBlitz followed by [ENTER] for the electrs service:"
-read PASSWORD_B
-
-# sudo nano /etc/systemd/system/electrs.service 
-echo "
-[Unit]
-Description=Electrs
-After=bitcoind.service
-
-[Service]
-WorkingDirectory=/home/admin/electrs
-ExecStart=/home/admin/electrs/target/release/electrs --index-batch-size=10 --jsonrpc-import --db-dir /mnt/hdd/electrs/db  --electrum-rpc-addr="0.0.0.0:50001" --cookie="raspibolt:$PASSWORD_B"
-
-User=admin
-Group=admin
-Type=simple
-KillMode=process
-TimeoutSec=60
-Restart=always
-RestartSec=60
-
-[Install]
-WantedBy=multi-user.target
-" | sudo tee -a /etc/systemd/system/electrs.service 
-
-sudo systemctl enable electrs
-sudo systemctl start electrs
-
-echo ""
-echo "***"
 echo "Please type the domain/ddns you have generated the certificate for followed by [ENTER]"
 read YOUR_DOMAIN
 
