0c055b9d6763c446488a31c7fffc716ccaf1b5ae
openoms <openoms@riseup.net>
2019-08-05 07:23:27 +0100

    bump electrum to 3.3.8

--- electrs/4_electrum_install.sh
+++ electrs/4_electrum_install.sh
@@ -5,13 +5,13 @@
 # Install dependencies: 	
 sudo apt-get install -y python3-pyqt5
 # Download package: 	
-wget https://download.electrum.org/3.3.4/Electrum-3.3.4.tar.gz
+wget https://download.electrum.org/3.3.8/Electrum-3.3.8.tar.gz
 
 #Verify signature:
 https://raw.githubusercontent.com/spesmilo/electrum/master/pubkeys/ThomasV.asc
 gpg --import ThomasV.asc
-wget https://download.electrum.org/3.3.4/Electrum-3.3.4.tar.gz.asc
-verifyResult=$(gpg --verify Electrum-3.3.4.tar.gz.asc 2>&1)
+wget https://download.electrum.org/3.3.8/Electrum-3.3.8.tar.gz.asc
+verifyResult=$(gpg --verify Electrum-3.3.8.tar.gz.asc 2>&1)
 goodSignature=$(echo ${verifyResult} | grep 'Good signature' -c)
 echo "goodSignature(${goodSignature})"
 if [ ${goodSignature} -lt 1 ]; then
@@ -20,11 +20,11 @@ if [ ${goodSignature} -lt 1 ]; then
   exit 1
 fi
 
-# Run without installing: 	tar -xvf Electrum-3.3.4.tar.gz
-# python3 Electrum-3.3.4/run_electrum
+# Run without installing: 	tar -xvf Electrum-3.3.8.tar.gz
+# python3 Electrum-3.3.8/run_electrum
 # Install with PIP: 	
 sudo apt-get install -y python3-setuptools python3-pip
-python3 -m pip install --user Electrum-3.3.4.tar.gz[fast]
+python3 -m pip install --user Electrum-3.3.8.tar.gz[fast]
 
 # add install dir to PATH (and make persist)
 PATH=$PATH:~/.local/bin
@@ -32,23 +32,22 @@ touch ~/.profile
 export PATH
 ~/.profile
 
-echo "Type the PASSWORD B of your RaspiBlitz followed by [ENTER]:"
-read PASSWORD_B
 echo "Type the LAN IP ADDRESS of your RaspiBlitz followed by [ENTER]:"
 read RASPIBLITZ_IP
 
 # Make Electrum config persist (editing ~/.electrum/config)
 # sudo nano ~/.electrum/config
-#     "oneserver": true,
-#     "rpcpassword": "$PASSWORD_B",
 #     "rpcuser": "raspibolt",
 #     "server": "192.168.1.239:50001:t",
 electrum setconfig oneserver true
-electrum setconfig rpcpassword $PASSWORD_B
-electrum setconfig rpcuser raspibolt
 electrum setconfig server $RASPIBLITZ_IP:50001:t
 
 electrum --oneserver --server $RASPIBLITZ_IP:50001:t 
 
-# to connect through SSL:
-# electrum --oneserver --server $YOUR_DOMAIN:50002:s
+echo "To start again: run \`electrum\` in the terminal."
+
+echo "To connect through SSL:"
+echo "Run: \`electrum --oneserver --server $YOUR_DOMAIN:50002:s\`"
+echo "edit ~/.electrum/config: \"server\": \"<your_domain_or_dynDNS>:50002:s\""
+
+electrum --oneserver --server $RASPIBLITZ_IP:50001:t 
