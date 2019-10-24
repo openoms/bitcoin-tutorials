4aa1ab954f5dcaa9409287a59ee3556a6cc973d2
openoms <43343391+openoms@users.noreply.github.com>
2019-10-24 10:15:45 +0100

    electrs: force Rust 1.37

--- electrs/electrs_to_RaspiBlitz.sh
+++ electrs/electrs_to_RaspiBlitz.sh
@@ -25,7 +25,7 @@ echo ""
 sudo -u electrs curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo -u electrs sh -s -- -y
 # workaround to keep Rust at v1.37.0
 # check rust version with: $ sudo -u electrs /home/electrs/.cargo/bin/cargo --version
-sudo -u electrs /home/electrs/.cargo/bin/rustup install 1.37.0
+sudo -u electrs /home/electrs/.cargo/bin/rustup install 1.37.0 --force
 sudo -u electrs /home/electrs/.cargo/bin/rustup override set 1.37.0
 
 #source $HOME/.cargo/env
