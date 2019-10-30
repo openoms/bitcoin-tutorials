94e1004d22c1a0c09e8d2429a7553eb9436aa7bc
openoms <oms@tuta.io>
2019-10-30 12:28:19 +0000

    electrs: clarify ports in script output

--- electrs/electrs_to_RaspiBlitz.sh
+++ electrs/electrs_to_RaspiBlitz.sh
@@ -267,22 +267,21 @@ if [ "${runBehindTor}" = "on" ]; then
     isElectrsTor=$(sudo cat /etc/tor/torrc 2>/dev/null | grep -c 'electrs')
     if [ ${isElectrsTor} -eq 0 ]; then
         echo "
-        # Hidden Service for Electrum Server
-        HiddenServiceDir /mnt/hdd/tor/electrs
-        HiddenServiceVersion 3
-        HiddenServicePort 50002 127.0.0.1:50002
+# Hidden Service for Electrum Server
+HiddenServiceDir /mnt/hdd/tor/electrs
+HiddenServiceVersion 3
+HiddenServicePort 50002 127.0.0.1:50002
         " | sudo tee -a /etc/tor/torrc
 
         sudo systemctl restart tor
-        sudo systemctl restart tor@default
     fi
     TOR_ADDRESS=$(sudo cat /mnt/hdd/tor/electrs/hostname)
     echo ""
     echo "***"
     echo "The Tor Hidden Service address for electrs is:"
     echo "$TOR_ADDRESS"
-    echo "Electrum wallet: to connect through Tor open the Tor Browser  and start with the options:" 
-    echo "\`electrum --oneserver --server=$TOR_ADDRESS:50002:t --proxy socks5:127.0.0.1:9150\'"
+    echo "Electrum wallet: to connect through Tor open the Tor Browser and start with the options:" 
+    echo "\`electrum --oneserver --server=$TOR_ADDRESS:50002:s --proxy socks5:127.0.0.1:9150\`"
     echo "***"
     echo "" 
 fi
