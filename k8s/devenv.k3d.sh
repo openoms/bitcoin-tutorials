a4fca3fd95b966e22904b6d5699bc9003f53beee
openoms <oms@tuta.io>
2022-06-15 08:58:47 +0100

    k3d: use blitz.docker.sh if present, add tmux, fzf

--- k8s/devenv.k3d.sh
+++ k8s/devenv.k3d.sh
@@ -9,16 +9,48 @@ devenv.k3d.sh [on|off]"
 }
 
 function setup_devenv_k3d() {
-# dedicated user
-sudo adduser --disabled-password --gecos "" k3d
-sudo usermod -aG sudo k3d
-# aliases and fzf
-echo '\
-/usr/share/doc/fzf/examples/key-bindings.bashrc
-/usr/share/doc/fzf/examples/completion.bash
-export KUBE_CONFIG_PATH=~/.kube/config' \
- | sudo -u k3d tee -a /home/k3d/.bashrc
+  # dedicated user
+  sudo adduser --disabled-password --gecos "" k3d
+  sudo usermod -aG sudo k3d
 
+  # tools
+  sudo apt update
+  sudo apt install -y git tmux
+
+  # fzf
+  sudo -u k3d sh -c 'git clone --depth 1 https://github.com/junegunn/fzf.git /home/k3d/.fzf; /home/k3d/.fzf/install'
+
+  # kubectl
+  kubectl_version="1.24.1"
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin
+
+  # bitcoin
+  bitcoin_version="22.0"
+  wget https://bitcoincore.org/bin/bitcoin-core-${bitcoin_version}/bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
+    && tar -xvf bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
+    && sudo mv bitcoin-${bitcoin_version}/bin/* /usr/local/bin
+
+  # docker
+  # look for raspiblitz install script
+  if [ -f /home/admin/config.scripts/blitz.docker.sh ]; then
+    /home/admin/config.scripts/blitz.docker.sh on
+  else
+    # https://docs.docker.com/desktop/linux/install/debian/
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+  fi
+  sudo groupadd docker
+  sudo usermod -aG docker k3d
+
+  # k3d
+  wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+  # KUBE_CONFIG_PATH
+  echo 'export KUBE_CONFIG_PATH=~/.kube/config' \
+ | sudo -u k3d tee -a /home/k3d/.bashrc
+# aliases
 echo "\
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
@@ -32,39 +64,19 @@ alias ll='ls -alF'
 alias ls='ls --color=auto'
 alias tf='terraform'" \
  | sudo -u k3d tee -a /home/k3d/.bash_aliases
+}
 
-# kubectl
-kubectl_version="1.24.1"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin
+function start_dev_charts() {
+  # starting the charts
+  cd /home/k3d/
+  sudo -u k3d git clone  https://github.com/GaloyMoney/charts
+  cd /home/k3d/charts/dev
 
-# bitcoin
-bitcoin_version="22.0"
-wget https://bitcoincore.org/bin/bitcoin-core-${bitcoin_version}/bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
-  && tar -xvf bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
-  && sudo mv bitcoin-${bitcoin_version}/bin/* /usr/local/bin
-
-# docker
-# https://docs.docker.com/desktop/linux/install/debian/
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-sudo groupadd docker
-sudo usermod -aG docker k3d
-
-# k3d
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
-# starting the charts
-cd /home/k3d/
-sudo -u k3d git clone  https://github.com/GaloyMoney/charts
-cd /home/k3d/charts/dev
-
-sudo -u k3d direnv allow
-sudo -u k3d make create-cluster
-sudo -u k3d make init
-sudo -u k3d bash -c 'export KUBE_CONFIG_PATH=~/.kube/config; make deploy-services'
-sudo -u k3d bash -c 'export KUBE_CONFIG_PATH=~/.kube/config; make deploy'
+  sudo -u k3d direnv allow
+  sudo -u k3d make create-cluster
+  sudo -u k3d make init
+  sudo -u k3d bash -c 'export KUBE_CONFIG_PATH=~/.kube/config; make deploy-services'
+  sudo -u k3d bash -c 'export KUBE_CONFIG_PATH=~/.kube/config; make deploy'
 }
 
 function delete_cluster() {
@@ -74,9 +86,10 @@ function delete_cluster() {
   # k3d cluster delete && rm terraform.tfstate
 }
 
-if [ "$1" = "1" ] || [ "$1" = "on" ]; then
+if [ "$1" = "on" ]; then
   setup_devenv_k3d
-elif [ "$1" = "0" ] || [ "$1" = "off" ]; then
+  start_dev_charts
+elif [ "$1" = "off" ]; then
   delete_cluster
 else
   help
