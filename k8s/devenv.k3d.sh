4b245a0c542e6724a1fa8d2aa66a1c1fe7bd24eb
openoms <oms@tuta.io>
2022-06-16 08:50:51 +0100

    k3d devenv: add terraform and checks for tools

--- k8s/devenv.k3d.sh
+++ k8s/devenv.k3d.sh
@@ -21,31 +21,47 @@ function setup_devenv_k3d() {
   sudo -u k3d sh -c 'git clone --depth 1 https://github.com/junegunn/fzf.git /home/k3d/.fzf; /home/k3d/.fzf/install'
 
   # kubectl
-  kubectl_version="1.24.1"
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl
-  chmod +x ./kubectl
-  sudo mv ./kubectl /usr/local/bin
+  if ! kubectl version; then
+    kubectl_version="1.24.1"
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin
+  fi
+
+  # terraform
+  if ! terraform version; then
+    if ! sudo apt install terraform; then
+      curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+      sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+      sudo apt-get update && sudo apt-get install terraform
+    fi
+  fi
 
   # bitcoin
   bitcoin_version="22.0"
-  wget https://bitcoincore.org/bin/bitcoin-core-${bitcoin_version}/bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
-    && tar -xvf bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
-    && sudo mv bitcoin-${bitcoin_version}/bin/* /usr/local/bin
-
+  if ! bitcoind --version; then
+    wget https://bitcoincore.org/bin/bitcoin-core-${bitcoin_version}/bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
+      && tar -xvf bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
+      && sudo mv bitcoin-${bitcoin_version}/bin/* /usr/local/bin
+  fi
   # docker
-  # look for raspiblitz install script
-  if [ -f /home/admin/config.scripts/blitz.docker.sh ]; then
-    /home/admin/config.scripts/blitz.docker.sh on
-  else
-    # https://docs.docker.com/desktop/linux/install/debian/
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sh get-docker.sh
+  if ! docker version; then
+    # look for raspiblitz install script
+    if [ -f /home/admin/config.scripts/blitz.docker.sh ]; then
+      /home/admin/config.scripts/blitz.docker.sh on
+    else
+      # https://docs.docker.com/desktop/linux/install/debian/
+      curl -fsSL https://get.docker.com -o get-docker.sh
+      sh get-docker.sh
+    fi
   fi
   sudo groupadd docker
   sudo usermod -aG docker k3d
 
   # k3d
-  wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+  if ! k3d version; then
+    wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+  fi
 
   # KUBE_CONFIG_PATH
   echo 'export KUBE_CONFIG_PATH=~/.kube/config' \
