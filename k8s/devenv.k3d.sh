2e2ba78ccea2855eb7ca0ea52f8cebb18427c7c3
openoms <oms@tuta.io>
2022-07-06 21:17:21 +0100

    devenv: add RPi changes and notes

--- k8s/devenv.k3d.sh
+++ k8s/devenv.k3d.sh
@@ -15,35 +15,54 @@ function setup_devenv_k3d() {
 
   # tools
   sudo apt update
-  sudo apt install -y git tmux
+  sudo apt install -y git tmux gnupg unzip curl
 
   # fzf
-  sudo -u k3d sh -c 'git clone --depth 1 https://github.com/junegunn/fzf.git /home/k3d/.fzf; /home/k3d/.fzf/install'
+  sudo -u k3d sh -c 'git clone --depth 1 https://github.com/junegunn/fzf.git /home/k3d/.fzf; /home/k3d/.fzf/install --all'
+
+  cpu=$(dpkg --print-architecture)
 
   # kubectl
   if ! kubectl version; then
     kubectl_version="1.24.1"
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/${cpu}/kubectl
     chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin
   fi
 
+
   # terraform
   if ! terraform version; then
-    if ! sudo apt install terraform; then
-      curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-      sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-      sudo apt-get update && sudo apt-get install terraform
-    fi
+      if [ "${cpu}" = amd64 ]; then
+        if ! sudo apt install terraform; then
+          sudo apt-get install -y software-properties-common
+          curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+          sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+          sudo apt-get update
+          sudo apt-get install -y terraform
+        fi
+      elif [ "${cpu}" = arm64 ]; then
+        # RPI
+        wget -O terraform_1.2.4_linux_arm64.zip https://releases.hashicorp.com/terraform/1.2.4/terraform_1.2.4_linux_arm64.zip || exit 1
+        wget https://releases.hashicorp.com/terraform/1.2.4/terraform_1.2.4_SHA256SUMS
+        sha256sum -c terraform_1.2.4_SHA256SUMS --ignore-missing || exit 1
+        wget https://releases.hashicorp.com/terraform/1.2.4/terraform_1.2.4_SHA256SUMS.sig || exit 1
+        gpg --verify terraform_1.2.4_SHA256SUMS.sig || exit 1
+        unzip terraform_1.2.4_linux_arm64.zip
+        sudo mv ./terraform /usr/local/bin/
+      fi
   fi
 
   # bitcoin
   bitcoin_version="22.0"
   if ! bitcoind --version; then
-    wget https://bitcoincore.org/bin/bitcoin-core-${bitcoin_version}/bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
-      && tar -xvf bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
-      && sudo mv bitcoin-${bitcoin_version}/bin/* /usr/local/bin
+    if [ "${cpu}" = amd64 ]; then
+      wget https://bitcoincore.org/bin/bitcoin-core-${bitcoin_version}/bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
+        && tar -xvf bitcoin-${bitcoin_version}-x86_64-linux-gnu.tar.gz \
+        && sudo mv bitcoin-${bitcoin_version}/bin/* /usr/local/bin
+    fi
   fi
+
   # docker
   if ! docker version; then
     # look for raspiblitz install script
@@ -110,3 +129,8 @@ elif [ "$1" = "off" ]; then
 else
   help
 fi
+
+# RPi problems:
+# k -n galoy-dev-otel logs opentelemetry-collector-675b6bb545-ss4mc
+# standard_init_linux.go:228: exec user process caused: exec format error
+# https://github.com/GaloyMoney/galoy/pull/1209#issuecomment-1098095373
